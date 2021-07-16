6ac5eb855a87c5c7c87a7a6035071a825a41acda
Henry Jiang <njjza@hotmail.com>
2021-07-16 11:33:58 -0700

    Software Pipelining Rewrite

--- include/libs/Cordic/Cordic.c
+++ include/libs/Cordic/Cordic.c
@@ -35,7 +35,7 @@ void cordic(struct Vector * v)
 
 void cordic_kernel(int* x, int* y, int* z) 
 {
-    int x_temp_1, y_temp_1, z_temp;
+    int x_temp_1, y_temp_1, z_temp, temp;
     int x_temp_2, y_temp_2;
     int i, mul;
 
@@ -43,30 +43,34 @@ void cordic_kernel(int* x, int* y, int* z)
     y_temp_1 = *y;
     z_temp = *z;
 
+    temp = AngleTable[0];
     #ifndef CORDIC_KERNEL
     #define CORDIC_KERNEL(z, x, c, y, i) (z = x + c * (y >> i))
     for( i=0; i < PRECISION; i+=4) {
         mul = (y_temp_1 >= 0) ? 1 : -1;
-        
         CORDIC_KERNEL(x_temp_2, x_temp_1, mul, y_temp_1, i);
         CORDIC_KERNEL(y_temp_2, y_temp_1, (~mul + 1), x_temp_1, i);
-        z_temp += mul * AngleTable[i];
+        z_temp += mul * temp;
 
+        temp = AngleTable[i + 1];
         mul = (y_temp_2 >= 0) ? 1 : -1;
         CORDIC_KERNEL(x_temp_1, x_temp_2, mul, y_temp_2, (i+1));
         CORDIC_KERNEL(y_temp_1, y_temp_2, (~mul + 1), x_temp_2, (i+1));
-        z_temp += mul * AngleTable[i+1];
+        z_temp += mul * temp;
 
+        temp = AngleTable[i + 2];
         mul = (y_temp_1 >= 0) ? 1 : -1;
-        
         CORDIC_KERNEL(x_temp_2, x_temp_1, mul, y_temp_1, (i+2));
         CORDIC_KERNEL(y_temp_2, y_temp_1, (~mul + 1), x_temp_1, (i+2));
-        z_temp += mul * AngleTable[i+2];
+        z_temp += mul * temp;
 
+        temp = AngleTable[i + 3];
         mul = (y_temp_2 >= 0) ? 1 : -1;
         CORDIC_KERNEL(x_temp_1, x_temp_2, mul, y_temp_2, (i+3));
         CORDIC_KERNEL(y_temp_1, y_temp_2, (~mul + 1), x_temp_2, (i+3));
-        z_temp += mul * AngleTable[i+3];
+        z_temp += mul * temp;
+
+        temp = AngleTable[i + 4];
     }
     #undef CORDIC_KERNEL
     #endif
