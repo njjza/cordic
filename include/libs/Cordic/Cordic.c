afb61a1f0c33c3a221f37f403b5dae00bab1e9d3
Henry Jiang <njjza@hotmail.com>
2021-07-16 11:31:04 -0700

    Loop Unrolling

--- include/libs/Cordic/Cordic.c
+++ include/libs/Cordic/Cordic.c
@@ -23,9 +23,14 @@ void cordic(struct Vector * v)
     y_i >>= EXTENTION;
     z_i >>= EXTENTION;
 
+    /*
     v -> x = (double) x_i / (1 << DESIRED_PRECISION);
     v -> y = (double) y_i / (1 << DESIRED_PRECISION);
     v -> z = (double) z_i / (1 << DESIRED_PRECISION);
+    */
+    v -> x = (double) x_i;
+    v -> y = (double) y_i;
+    v -> z = (double) z_i;
 }
 
 void cordic_kernel(int* x, int* y, int* z) 
@@ -40,14 +45,28 @@ void cordic_kernel(int* x, int* y, int* z)
 
     #ifndef CORDIC_KERNEL
     #define CORDIC_KERNEL(z, x, c, y, i) (z = x + c * (y >> i))
-    for( i=0; i < PRECISION; i++) {
+    for( i=0; i < PRECISION; i+=4) {
         mul = (y_temp_1 >= 0) ? 1 : -1;
+        
         CORDIC_KERNEL(x_temp_2, x_temp_1, mul, y_temp_1, i);
         CORDIC_KERNEL(y_temp_2, y_temp_1, (~mul + 1), x_temp_1, i);
-        z_temp += AngleTable[i];
-            
-        x_temp_1 = x_temp_2;
-        y_temp_1 = y_temp_2;
+        z_temp += mul * AngleTable[i];
+
+        mul = (y_temp_2 >= 0) ? 1 : -1;
+        CORDIC_KERNEL(x_temp_1, x_temp_2, mul, y_temp_2, (i+1));
+        CORDIC_KERNEL(y_temp_1, y_temp_2, (~mul + 1), x_temp_2, (i+1));
+        z_temp += mul * AngleTable[i+1];
+
+        mul = (y_temp_1 >= 0) ? 1 : -1;
+        
+        CORDIC_KERNEL(x_temp_2, x_temp_1, mul, y_temp_1, (i+2));
+        CORDIC_KERNEL(y_temp_2, y_temp_1, (~mul + 1), x_temp_1, (i+2));
+        z_temp += mul * AngleTable[i+2];
+
+        mul = (y_temp_2 >= 0) ? 1 : -1;
+        CORDIC_KERNEL(x_temp_1, x_temp_2, mul, y_temp_2, (i+3));
+        CORDIC_KERNEL(y_temp_1, y_temp_2, (~mul + 1), x_temp_2, (i+3));
+        z_temp += mul * AngleTable[i+3];
     }
     #undef CORDIC_KERNEL
     #endif
